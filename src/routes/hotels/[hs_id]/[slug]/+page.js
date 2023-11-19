b5f6d4e517bd80a3c7e2b734da450d3b088d71ac
Ahmed Aisar <aisar@atmospherehotelsandresorts.net>
2023-11-19 14:44:45 +0500

    update#34

--- src/routes/hotels/[hs_id]/[slug]/+page.js
+++ src/routes/hotels/[hs_id]/[slug]/+page.js
@@ -1,6 +1,6 @@
 export const load = async ({ fetch, params }) => {
     let para = params    
-    let req = await fetch("https://nodeapi-506j.onrender.com/scan?hotelid={}&checkin=2024-01-17&checkout=2024-01-21")
+    let req = await fetch("https://nodeapi-506j.onrender.com/scan?hotelid=690385651&checkin=2024-01-17&checkout=2024-01-21");
     let res = await req.json();     
 	
     return {
