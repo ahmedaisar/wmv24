fd6a571187ca6486bbdfbc47717f1a328ef231d5
Ahmed Aisar <aisar@atmospherehotelsandresorts.net>
2023-11-19 12:53:39 +0500

    update#19

--- src/routes/hotels/[hs_id]/[slug]/+page.js
+++ src/routes/hotels/[hs_id]/[slug]/+page.js
@@ -1,6 +1,12 @@
 export const load = async ({ fetch, params }) => {
+    const useragent = require('new-user-agent')
+    let headers = new Headers({
+        "Accept"       : "application/json",
+        "Content-Type" : "application/json",
+        "User-Agent"   : useragent()
+    });
     let para = params    
-    let req = await fetch("https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-01-17&checkout=2024-01-21&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0", { agent });
+    let req = await fetch("https://hotelscan.com/combiner?pos=zz&locale=en&checkin=2024-01-17&checkout=2024-01-21&rooms=2&mobile=0&loop=1&country=MV&ef=1&geoid=xmmmamtksdxx&availability=1&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0", { headers });
     let res = await req.json();     
 	
     return {
