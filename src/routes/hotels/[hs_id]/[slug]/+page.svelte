d2afdfa8156049cc32557955a50db491650fd17d
Ahmed Aisar <aisar@atmospherehotelsandresorts.net>
2023-11-19 15:59:29 +0500

    update#50

--- src/routes/hotels/[hs_id]/[slug]/+page.svelte
+++ src/routes/hotels/[hs_id]/[slug]/+page.svelte
@@ -34,8 +34,8 @@
     async function getHotelPromise(){
         const req = await fetch("https://nodeapi-506j.onrender.com/scan?hotelid=690385651&checkin=2024-01-17&checkout=2024-01-21")
         const res = await req.json()
-        // const dat = JSON.stringify(res)
-        // const jsn = JSON.parse(dat)
+        JSON.parse(res)
+        const jsn = JSON.parse(res)
         console.log(res)
         return jsn 
     }
