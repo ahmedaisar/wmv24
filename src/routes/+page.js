2a51035cce08ae2b9c58a7c9fe46a70471d26d19
Aisar <ahmed.aisar@gmail.com>
2024-08-21 16:06:36 +0500

    update#6

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,6 @@
 export const load = async ({  }) => {
 
-    const req = await fetch(`https://www.traveltrademaldives.com/wp-json/wp/v2/posts?categories=2&_fields=link,date,title,_embedded&_embed`)
+    const req = await fetch(`https://www.traveltrademaldives.com/wp-json/wp/v2/posts?categories=2&_fields=title,date,_links,_embedded&_embed`)
     const res = await req.json()
       
     return {
