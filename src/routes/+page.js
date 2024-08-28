e13b395eaf41e6fce1e53982d35ae33ac0ba733f
Aisar <ahmed.aisar@gmail.com>
2024-08-28 11:15:31 +0500

    #update11-added-apis-home

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,11 +1,24 @@
-export const load = async ({  }) => {
+export const load = async ({ params  }) => {
 
-    const req = await fetch(`https://www.traveltrademaldives.com/wp-json/wp/v2/posts?categories=2&_fields=title,date,_links,_embedded&_embed`)
-    const res = await req.json()
-      
-    return {
-        posts: res,
+    const { checkin, checkout, adults, child } = params
+
+    const blog = await fetch(`https://www.traveltrademaldives.com/wp-json/wp/v2/posts?categories=2&_fields=title,date,_links,_embedded&_embed`)
+    const blogres = await blog.json()
+
+    const hotels = await fetch(`https://letsgo-seven.vercel.app/api/hotels?checkin${checkin}&checkout=${checkout}&adults=${adults}&child=${child}`)
+    const hotelres = await blog.json()
+
+    if (!params) {
+        return {
+            posts: blogres,
+        }
+        
     }
+    return {
+        posts: blogres,
+        hotels: hotelres
+    }
+         
 }
 
  
