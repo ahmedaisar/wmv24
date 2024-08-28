ba2531442d26d348201950422ad6a6ed96ba3957
Aisar <ahmed.aisar@gmail.com>
2024-08-28 14:39:47 +0500

    #update21

--- src/routes/hotels/+page.js
+++ src/routes/hotels/+page.js
@@ -2,24 +2,21 @@ import * as maldives from '$lib/data/maldives.json';
 
 export const load = async ({ params }) => {
 	const { checkin, checkout, adults, child } = params;
+	let data;
 
-	const hotels = await fetch(
-		`https://letsgo-seven.vercel.app/api/hotels?checkin${checkin}&checkout=${checkout}&adults=${adults}&child=${child}`
-	);
-	const hotelres = await hotels.json();
+	if (params) {
+		const hotels = await fetch(
+			`https://letsgo-seven.vercel.app/api/hotels?checkin${checkin}&checkout=${checkout}&adults=${adults}&child=${child}`
+		);
+		data = await hotels.json();
+	}
+	const localdata = maldives?.data?.records;
 
-	const data = maldives?.data?.records;
-
-	const resorts = data.filter((h) => {
+	data = localdata.filter((h) => {
 		return h.toa_label === 'resort' && h.quality.stars == 5;
 	});
 
-	if (!params) {
-		return {
-			hotels: resorts
-		};
-	}
 	return {
-		hotels: hotelres
+		hotels: data
 	};
 };
